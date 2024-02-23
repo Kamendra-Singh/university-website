3bff62f4650980613b50b5be6428f96ebb7336cd
Kamendra-Singh <154981054+Kamendra-Singh@users.noreply.github.com>
2024-02-23 17:52:14 +0530

    Add files via upload

---
+++ project univ/js/app.js
@@ -0,0 +1,8 @@
+/* java script for toggle menu */
+function hideMenu(){
+    let nav=document.getElementById("nav-links").style.right="-200px";
+}
+
+function showMenu(){
+    let nav=document.getElementById("nav-links").style.right="0";
+}
